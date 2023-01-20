c1f5906375e3652a3f4980a6a3f4fcfd0f9ea559
Dharmendra Yadav <you@example.com>
2023-01-20 07:28:55 +0530

    Final Code

---
+++ src/files/ReviewList.js
@@ -0,0 +1,65 @@
+const ReviewList = 
+[
+    {
+        id:1,
+        name: "Roger",
+        rating: 3,
+        comment: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,"
+    },
+    {
+        id:2,
+        name: "Mark",
+        rating: 4,
+        comment: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,"
+    },
+    {
+        id:3,
+        name: "Raj",
+        rating: 2,
+        comment: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,"
+    },
+    {
+        id:4,
+        name: "Tom",
+        rating: 2,
+        comment: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,"
+    },
+    {
+        id:5,
+        name: "Stacy",
+        rating: 3,
+        comment: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,"
+    },
+    {
+        id:6,
+        name: "Albert",
+        rating: 5,
+        comment: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,"
+    },
+    {
+        id:7,
+        name: "Will",
+        rating: 2,
+        comment: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,"
+    },
+    {
+        id:8,
+        name: "Ashley",
+        rating: 3,
+        comment: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,"
+    },
+    {
+        id:9,
+        name: "Rose",
+        rating: 3,
+        comment: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,"
+    },
+    {
+        id:10,
+        name: "Smith",
+        rating: 4,
+        comment: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,"
+    }
+];
+
+export default ReviewList;
